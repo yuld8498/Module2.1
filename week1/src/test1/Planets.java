e977e9581acb5b3ff9645ce566fe03c5faba1f88
yuld8498 <truongthanh11201@gmail.com>
2022-06-03 15:05:42 +0700

    add

---
+++ week1/src/test1/Planets.java
@@ -0,0 +1,10 @@
+package test1;
+
+import java.util.Date;
+
+public class Planets {
+    public static void main(String[] daytime){
+        Date now = new Date();
+        System.out.println("Now: "+now);
+    }
+}
